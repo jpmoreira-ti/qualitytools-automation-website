1bf2412040aafc8caecd22dd62fd5f675e4de010
jpmoreira-ti <jpmoreira.ti@gmail.com>
2024-12-26 18:14:03 -0300

    [tec] final adjusts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ const apiTemplates = [
   },
   {
     id: "python",
-    name: "Robot F + Requests",
+    name: "Robot F. with Requests",
     link: "https://github.com/jpmoreira-ti/template-python-api-tests",
     imgSrc: "/img/python-logo.png",
     alt: "Python API Template Repository",
@@ -39,14 +39,14 @@ const webTemplates = [
     id: "cypress",
     name: "Cypress",
     link: "https://github.com/jpmoreira-ti/template-web-api-cypress-tests",
-    imgSrc: "/img/cypress.png",
+    imgSrc: "/img/cypress-logo.png",
     alt: "Cypress Web API Template Repository",
   },
   {
     id: "robot-web",
-    name: "Robot F. + Selenium",
+    name: "Robot F. with Selenium",
     link: "https://github.com/jpmoreira-ti/template-web-robot-framework",
-    imgSrc: "/img/robotf.png",
+    imgSrc: "/img/robotf-logo.png",
     alt: "Robot Framework Web Template Repository",
   },
 ];
@@ -56,7 +56,17 @@ const mobileTemplates = [
     id: "Compose-ui",
     name: "Instrumented Tests with Compose UI",
     link: "https://github.com/jpmoreira-ti/template-compose-ui-tests",
-    imgSrc: "/img/compose-ui.png",
+    imgSrc: "/img/compose-ui-logo.png",
+    alt: "Android Compose Repository",
+  }
+];
+
+const perfomanceTemplates = [
+  {
+    id: "k6-perfomance",
+    name: "Load Tests with K6",
+    link: "https://github.com/jpmoreira-ti/template-grafana-k6-load-test",
+    imgSrc: "/img/k6-logo.png",
     alt: "Android Compose Repository",
   }
 ];
@@ -76,15 +86,19 @@ export default function Home() {
     template.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  const filteredPerfomanceTemplates = perfomanceTemplates.filter((template) =>
+    template.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 sm:p-20 gap-8">
       <main className="flex flex-col gap-8 items-center w-full max-w-4xl">
         <h1 className="text-4xl font-bold text-center mt-4">Quality Tools Automation</h1>
         <p className="text-center text-lg">
-          Welcome to my template repository! I&apos;m João Paulo Moreira and I&apos;ve been working as a SDET over 6 years. This page is dedicated to providing templates for API test automation, mobile test automation, and performance testing. Browse through the templates below and click on the links to access the corresponding GitHub repositories. I hope it can helping you in your test automation journey! 
+          Welcome to my template repository! I&apos;m João Paulo Moreira and I&apos;ve been working as a SDET for over 6 years. This page is dedicated to providing templates for API, Web and Mobile automation testing and performance/load testing. Browse through the templates below and click on the links to access the corresponding GitHub repositories. I hope it can helping you in your test automation journey! 
         </p>
         <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-        <h1 className="text-2xl font-bold text-center mt-4">Templates API</h1>
+        <h1 className="text-2xl font-bold text-center mt-4">Templates API Tests</h1>
         <hr className="w-full border-t border-gray-300 my-4" />
         <div className="grid grid-cols-2 gap-8 mt-4 w-full">
         {filteredApiTemplates.map((template, index) => (
@@ -106,7 +120,7 @@ export default function Home() {
         ))}
       </div>
         <hr className="w-full border-t border-gray-300 my-8" />
-        <h1 className="text-2xl font-bold text-center mt-4">Templates WEB</h1>
+        <h1 className="text-2xl font-bold text-center mt-4">Templates WEB Tests</h1>
         <div className="grid grid-cols-2 gap-8 mt-4 w-full">
           {filteredWebTemplates.map((template, index) => (
             <div
@@ -127,9 +141,30 @@ export default function Home() {
           ))}
         </div>
         <hr className="w-full border-t border-gray-300 my-8" />
-        <h1 className="text-2xl font-bold text-center mt-4">Templates MOBILE</h1>
+        <h1 className="text-2xl font-bold text-center mt-4">Templates MOBILE Tests</h1>
         <div className="grid grid-cols-2 gap-8 mt-4 w-full">
           {filteredMobileTemplates.map((template, index) => (
+            <div
+            key={template.id}
+            className={`flex flex-col items-center text-center ${index === 1 ? 'mb-16' : ''}`}
+          >
+              <h2 id={template.id} className="text-xl font-bold mb-2">{template.name}</h2>
+              <a href={template.link} target="_blank" rel="noopener noreferrer">
+                <img
+                  src={template.imgSrc}
+                  alt={template.alt}
+                  width={200}
+                  height={200}
+                  className="rounded-lg"
+                />
+              </a>
+            </div>
+          ))}
+        </div>
+        <hr className="w-full border-t border-gray-300 my-8" />
+        <h1 className="text-2xl font-bold text-center mt-4">Templates LOAD Tests</h1>
+        <div className="grid grid-cols-2 gap-8 mt-4 w-full">
+          {filteredPerfomanceTemplates.map((template, index) => (
             <div
             key={template.id}
             className={`flex flex-col items-center text-center ${index === 1 ? 'mb-16' : ''}`}
